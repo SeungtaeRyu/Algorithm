f5e16423977504e31fa45bfd62a782d857df3a54
Seungtae Ryu <81846487+SeungtaeRyu@users.noreply.github.com>
2022-08-20 23:06:17 +0900

    [Silver V] Title: 국회의원 선거, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
@@ -1,9 +1,7 @@
 import sys
 input = sys.stdin.readline
-
-n = int(input())                                # 4                     3
-vote = [int(input()) for _ in range(n)]         # [10, 10, 10, 10]      [5, 7, 7]
-
+n = int(input())
+vote = [int(input()) for _ in range(n)]
 count = 0
 while True:
     for i in range(1, len(vote)):
@@ -11,8 +9,7 @@ while True:
             break
     else:
         break
-    # print(vote.index(max(vote[1:]),1))
-    vote[vote.index(max(vote[1:]), 1)] -= 1
+    vote[vote.index(max(vote), 1)] -= 1
     vote[0] += 1
     count += 1
 print(count)
