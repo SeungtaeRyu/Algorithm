3502396ff349554486423bebea27546c54cb5bf4
Seungtae Ryu <81846487+SeungtaeRyu@users.noreply.github.com>
2022-08-28 12:22:47 +0900

    [Silver IV] Title: 상근이의 여행, Time: 276 ms, Memory: 32452 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -0,0 +1,25 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+T = int(input())
+for tc in range(1, T+1):
+    n, m = map(int, input().split())
+    adj_dict = {i: [] for i in range(1, n+1)}
+    visited = [False] * (1+n)
+    for i in range(m):
+        x, y = map(int, input().split())
+        adj_dict[x].append(y)
+        adj_dict[y].append(x)
+    q = deque()
+    q.append(1)
+    visited[1] = True
+    count = 0
+    while q:
+        v = q.popleft()
+        for w in adj_dict[v]:
+            if not visited[w]:
+                visited[w] = True
+                q.append(w)
+                count += 1
+    print(count)
