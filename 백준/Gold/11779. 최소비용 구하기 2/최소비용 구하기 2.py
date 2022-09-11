526dfa941c61f1634d6f01208cf942576610ec49
Seungtae Ryu <81846487+SeungtaeRyu@users.noreply.github.com>
2022-09-12 03:38:43 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 196 ms, Memory: 120596 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -4,40 +4,32 @@ input = sys.stdin.readline
 n = int(input())
 m = int(input())
 
-adj = {i: [] for i in range(n+1)}                   # adj 행렬 초기화
-routes = {i: [] for i in range(n+1)}
+adj = {i: [] for i in range(n+1)}
+routes = {i: [] for i in range(n+1)}                # routes 딕셔너리 선언
 for _ in range(m):
     x, y, p = map(int, input().split())
     adj[x].append((y, p))
 
-# print()
-# print(adj)
+dp = [sys.maxsize] * (n+1)
+visited = [False] * (n+1)
 
-dp = [sys.maxsize] * (n+1)                                  # dp 초기화
-visited = [False] * (n+1)                           # visit 초기화
+start, end = map(int, input().split())
 
-start, end = map(int, input().split())              # 시작점, 끝점 세팅
-
-stack = [start]                                                     # 시작점을 스택에 넣고 시작
-dp[start] = 0                                                       # 시작점 거리 비용 0으로 설정
-routes[start].append(start)
+stack = [start]
+dp[start] = 0
+routes[start] = [start]                        # start route 추가
 while stack:
-    now_node = stack.pop()                                          # 스택에 있는 노드를 pop 하고 "방문기록 체크"
+    now_node = stack.pop()
     visited[now_node] = True
-    for next_node, next_price in adj[now_node]:                                 # pop 한 노드에서 갈 수 있는 다음 노드 탐색 및 비용 계산
+    for next_node, next_price in adj[now_node]:
         if visited[next_node]:
             continue
         else:
-            # routes[next_node] = routes[now_node] + [next_node]
-            if dp[now_node] + next_price < dp[next_node]:
+            if dp[now_node] + next_price < dp[next_node]:               # dp[i]가 갱신 될 때마다
                 dp[next_node] = dp[now_node] + next_price
-                routes[next_node] = routes[now_node] + [next_node]
-            # dp[next_node] = min(dp[now_node] + next_price, dp[next_node])
+                routes[next_node] = routes[now_node] + [next_node]      # route[i]도 같이 갱신
 
-    # print(visited)
-    # print(dp)
-
-    next_index = 0                      # 방문한 노드를 제외하고 비용이 가장 적은 노드를 next 노드로 설정하기 위해 인덱스를 찾음
+    next_index = 0
     min_value = sys.maxsize
     for i in range(1, n+1):
         if visited[i]:
@@ -46,11 +38,9 @@ while stack:
             next_index = i
             min_value = dp[i]
 
-    if next_index != end:               # next 노드가 도착점이면 while 탈출
-        stack.append(next_index)        # 아니면 스택에 푸쉬하여 다시 while 문 반복
-
-    # print(routes)
+    if next_index != end:
+        stack.append(next_index)
 
 print(dp[end])
-print(len(routes[end]))
-print(*routes[end])
+print(len(routes[end]))             # 루트 길이 출력
+print(*routes[end])                 # 루트 경로 출력
