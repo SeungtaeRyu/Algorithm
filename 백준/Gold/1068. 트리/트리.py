f94060d8a67be43424f0c7adbd12485af5c857de
Seungtae Ryu <81846487+SeungtaeRyu@users.noreply.github.com>
2022-09-24 13:12:35 +0900

    [Gold V] Title: 트리, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/1068. 트리/트리.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+tree = {i: [] for i in range(n)}
+info = list(map(int, input().split()))
+delete_node = int(input())
+for i in range(n):
+    if info[i] == -1:
+        root = i
+        continue
+    if i == delete_node:
+        continue
+    tree[info[i]].append(i)
+
+if root == delete_node:
+    print(0)
+else:
+    visited = [False] * n
+    stack = [root]
+    ans = 0
+    while stack:
+        now_node = stack.pop()
+        if len(tree[now_node]) == 0:
+            ans += 1
+        for next_node in tree[now_node]:
+            stack.append(next_node)
+    print(ans)
+
+
