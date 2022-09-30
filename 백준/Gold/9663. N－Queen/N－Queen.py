1d4c0f6aa148a03cf55192d9bbb39fab76a24b0f
Seungtae Ryu <81846487+SeungtaeRyu@users.noreply.github.com>
2022-09-30 11:53:14 +0900

    [Gold IV] Title: N-Queen, Time: 7996 ms, Memory: 143700 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,26 +1,22 @@
-import sys
-input = sys.stdin.readline
+def Queens(floor ,checked, we_can, n):
+    global total
+    if floor == n:      # 끝까지 순열을 다 만들 수 있으면 +1 해준다
+        total += 1
 
-def f(i, k):
-    global count
-    for x in range(i-1):                                            # 가지치기
-        if p[i-1] == p[x] + (i-1-x) or p[i-1] == p[x] - (i-1-x):    # 대각선이 겹치면 잘라버리기
-            return
-    if i == k:              # 가지치기에 당하지 않고 i == k 까지 도착하면
-        count += 1          # count += 1
-    else:
-        for j in range(k):
-            if used[j] == 0:
-                used[j] = 1
-                p[i] = a[j]
-                f(i+1, k)
-                used[j] = 0
+    for i in range(len(we_can)):                        # 우리가 뽑을 수 있는 것중
+        cannot = False
+        for j in range(len(checked)):                   # 이미 한 것의 대각선에 걸리면 안된다.
+            if checked[j] + (floor - j) == we_can[i] or checked[j] - (floor - j) == we_can[i]:
+                cannot = True
+                break
+        if cannot:
+            continue
 
+        Queens(floor + 1, checked + [we_can[i]], we_can[:i] + we_can[i + 1:], n)
 
 N = int(input())
-count = 0
-a = [i for i in range(N)]
-p = [0] * N
-used = [0] * N
-f(0, N)
-print(f'{count}')
+
+total = 0
+arr = list(range(N))
+Queens(0, [], arr, N)
+print(f"{total}")
