257709d5b21ca128e4c62e69ccde3b958663eaf1
Seungtae Ryu <81846487+SeungtaeRyu@users.noreply.github.com>
2022-09-30 11:48:31 +0900

    [Gold IV] Title: N-Queen, Time: 10860 ms, Memory: 146000 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+def f(i, k):
+    global count
+    for x in range(i-1):                                            # 가지치기
+        if p[i-1] == p[x] + (i-1-x) or p[i-1] == p[x] - (i-1-x):    # 대각선이 겹치면 잘라버리기
+            return
+    if i == k:              # 가지치기에 당하지 않고 i == k 까지 도착하면
+        count += 1          # count += 1
+    else:
+        for j in range(k):
+            if used[j] == 0:
+                used[j] = 1
+                p[i] = a[j]
+                f(i+1, k)
+                used[j] = 0
+
+
+N = int(input())
+count = 0
+a = [i for i in range(N)]
+p = [0] * N
+used = [0] * N
+f(0, N)
+print(f'{count}')
