416b13a335a0f059f827074cc181430c058492d5
Seungtae Ryu <81846487+SeungtaeRyu@users.noreply.github.com>
2022-09-30 10:56:06 +0900

    [D3] Title: N-Queen, Time: 234 ms, Memory: 61,948 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.py
+++ SWEA/D3/2806. N－Queen/N－Queen.py
@@ -1,10 +1,10 @@
 def f(i, k):
     global count
-    for x in range(i-1):
-        if p[i-1] == p[x] + (i-1-x) or p[i-1] == p[x] - (i-1-x):
+    for x in range(i-1):                                            # 가지치기
+        if p[i-1] == p[x] + (i-1-x) or p[i-1] == p[x] - (i-1-x):    # 대각선이 겹치면 잘라버리기
             return
-    if i == k:
-        count += 1
+    if i == k:              # 가지치기에 당하지 않고 i == k 까지 도착하면
+        count += 1          # count += 1
     else:
         for j in range(k):
             if used[j] == 0:
@@ -17,7 +17,7 @@ T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     count = 0
-    a = [i for i in range(1,N+1)]
+    a = [i for i in range(N)]
     p = [0] * N
     used = [0] * N
     f(0, N)
