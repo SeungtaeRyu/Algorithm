9b096348a44024b32eae3a4333615a19b4052175
Seungtae Ryu <81846487+SeungtaeRyu@users.noreply.github.com>
2022-10-05 23:55:16 +0900

    [Gold III] Title: 줄 세우기, Time: 260 ms, Memory: 39288 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -0,0 +1,25 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+indegree = [0 for _ in range(n+1)]
+rules = {i: [] for i in range(1, n+1)}
+for _ in range(m):
+    a, b = map(int, input().split())
+    rules[a].append(b)
+    indegree[b] += 1
+
+q = deque()
+for i in range(1, n+1):
+    if indegree[i] == 0:
+        q.append(i)
+
+while q:
+    now = q.popleft()
+    print(now, end=" ")
+    for nextt in rules[now]:
+        indegree[nextt] -= 1
+        if indegree[nextt] == 0:
+            q.append(nextt)
