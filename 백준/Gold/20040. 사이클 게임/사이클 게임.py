bbb6d74bcc2f574096c694b807e18771256939c3
Seungtae Ryu <81846487+SeungtaeRyu@users.noreply.github.com>
2022-10-11 22:24:01 +0900

    [Gold IV] Title: 사이클 게임, Time: 2056 ms, Memory: 133528 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+rep = [i for i in range(n)]
+adj = {i: [i] for i in range(n)}
+
+flag = False
+ans = 0
+for _ in range(1, m+1):
+    a, b = map(int ,input().split())
+    if flag:
+        continue
+    if rep[a] == rep[b]:
+        ans = _
+        flag = True
+        continue
+
+    if rep[a] < rep[b]:
+        x = rep[b]
+        while adj[x]:
+            y = adj[x].pop()
+            adj[rep[a]].append(y)
+            rep[y] = rep[a]
+    else:
+        x = rep[a]
+        while adj[x]:
+            y = adj[x].pop()
+            adj[rep[b]].append(y)
+            rep[y] = rep[b]
+
+print(ans)
