842060861bdc0417587b846941e011e142f39036
Seungtae Ryu <81846487+SeungtaeRyu@users.noreply.github.com>
2022-10-30 12:56:35 +0900

    [Silver V] Title: Gravity Hackenbush, Time: 396 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/25335. Gravity Hackenbush/Gravity Hackenbush.py
+++ 백준/Silver/25335. Gravity Hackenbush/Gravity Hackenbush.py
@@ -7,18 +7,15 @@ for _ in range(n):
     input()
 
 R_count, G_count, B_count = 0, 0, 0
+count_dic = {'R': 0, 'G': 0, 'B': 0}
 
 for _ in range(m):
     v, w, c = input().split()
-    if c == 'R':
-        R_count += 1
-    elif c == 'G':
-        G_count += 1
-    elif c == 'B':
-        B_count += 1
 
-player1 = R_count + G_count // 2 + G_count % 2
-player2 = B_count + G_count // 2
+    count_dic[c] += 1
+
+player1 = count_dic['R'] + count_dic['G'] // 2 + count_dic['G'] % 2
+player2 = count_dic['B'] + count_dic['G'] // 2
 
 if player1 > player2:
     print('jhnah917')
