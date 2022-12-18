f7582269fa1aac79ae727737b4fa1e88c5539ef9
Seungtae Ryu <81846487+SeungtaeRyu@users.noreply.github.com>
2022-12-19 00:20:52 +0900

    [Gold V] Title: 경쟁적 전염, Time: 48 ms, Memory: 31080 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -7,41 +7,37 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 visited = [[0 for _ in range(n)] for _ in range(n)]
 s, a, b = map(int, input().split())
 
+stack = [(a-1, b-1)]
+ans = [0]
+count = 0
+while count < s and len(ans) == 1:
+    temp = []
+    while stack:
+        x, y = stack.pop()
+        if x - 1 >= 0 and visited[x - 1][y] == 0:
+            visited[x - 1][y] = 1
+            temp.append((x - 1, y))
+            if arr[x - 1][y] != 0:
+                ans.append(arr[x - 1][y])
+        if x + 1 < n and visited[x + 1][y] == 0:
+            visited[x + 1][y] = 1
+            temp.append((x + 1, y))
+            if arr[x + 1][y] != 0:
+                ans.append(arr[x + 1][y])
+        if y - 1 >= 0 and visited[x][y - 1] == 0:
+            visited[x][y - 1] = 1
+            temp.append((x, y - 1))
+            if arr[x][y - 1] != 0:
+                ans.append(arr[x][y - 1])
+        if y + 1 < n and visited[x][y + 1] == 0:
+            visited[x][y + 1] = 1
+            temp.append((x, y + 1))
+            if arr[x][y + 1] != 0:
+                ans.append(arr[x][y + 1])
+    stack = temp[:]
+    count += 1
 
-if arr[a-1][b-1] != 0:
-    print(arr[a-1][b-1])
+if len(ans) == 1:
+    print(0)
 else:
-    stack = [(a-1, b-1)]
-    ans = [0]
-    count = 0
-    while count < s and len(ans) == 1:
-        temp = []
-        while stack:
-            x, y = stack.pop()
-            if x - 1 >= 0 and visited[x - 1][y] == 0:
-                visited[x - 1][y] = 1
-                temp.append((x - 1, y))
-                if arr[x - 1][y] != 0:
-                    ans.append(arr[x - 1][y])
-            if x + 1 < n and visited[x + 1][y] == 0:
-                visited[x + 1][y] = 1
-                temp.append((x + 1, y))
-                if arr[x + 1][y] != 0:
-                    ans.append(arr[x + 1][y])
-            if y - 1 >= 0 and visited[x][y - 1] == 0:
-                visited[x][y - 1] = 1
-                temp.append((x, y - 1))
-                if arr[x][y - 1] != 0:
-                    ans.append(arr[x][y - 1])
-            if y + 1 < n and visited[x][y + 1] == 0:
-                visited[x][y + 1] = 1
-                temp.append((x, y + 1))
-                if arr[x][y + 1] != 0:
-                    ans.append(arr[x][y + 1])
-        stack = temp[:]
-        count += 1
-
-    if len(ans) == 1:
-        print(0)
-    else:
-        print(min(ans[1:]))
+    print(min(ans[1:]))
