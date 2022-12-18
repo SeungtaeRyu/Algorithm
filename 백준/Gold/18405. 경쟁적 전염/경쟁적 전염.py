eaafab956252bc74c751256abfae08f2f71a0f1f
Seungtae Ryu <81846487+SeungtaeRyu@users.noreply.github.com>
2022-12-19 00:19:49 +0900

    [Gold V] Title: 경쟁적 전염, Time: 48 ms, Memory: 31084 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -1,5 +1,4 @@
 import sys
-from collections import defaultdict
 
 input = sys.stdin.readline
 
@@ -8,41 +7,41 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 visited = [[0 for _ in range(n)] for _ in range(n)]
 s, a, b = map(int, input().split())
 
-virus = defaultdict(list)
-for i in range(n):
-    for j in range(n):
-        virus[arr[i][j]].append((i, j))
-        if arr[i][j] != 0:
-            visited[i][j] = 1
 
-virus = sorted(virus.items())[1:]
-
-count = 0
-while count < s:
-    for i in range(len(virus)):
-        v = virus[i][0]
-        v_list = virus[i][1]
-        # print(v, v_list)
+if arr[a-1][b-1] != 0:
+    print(arr[a-1][b-1])
+else:
+    stack = [(a-1, b-1)]
+    ans = [0]
+    count = 0
+    while count < s and len(ans) == 1:
         temp = []
-        while v_list:
-            x, y = v_list.pop()
+        while stack:
+            x, y = stack.pop()
             if x - 1 >= 0 and visited[x - 1][y] == 0:
                 visited[x - 1][y] = 1
-                arr[x - 1][y] = v
                 temp.append((x - 1, y))
+                if arr[x - 1][y] != 0:
+                    ans.append(arr[x - 1][y])
             if x + 1 < n and visited[x + 1][y] == 0:
                 visited[x + 1][y] = 1
-                arr[x + 1][y] = v
                 temp.append((x + 1, y))
+                if arr[x + 1][y] != 0:
+                    ans.append(arr[x + 1][y])
             if y - 1 >= 0 and visited[x][y - 1] == 0:
                 visited[x][y - 1] = 1
-                arr[x][y - 1] = v
                 temp.append((x, y - 1))
+                if arr[x][y - 1] != 0:
+                    ans.append(arr[x][y - 1])
             if y + 1 < n and visited[x][y + 1] == 0:
                 visited[x][y + 1] = 1
-                arr[x][y + 1] = v
                 temp.append((x, y + 1))
-        virus[i] = (v, temp[:])
-    count += 1
+                if arr[x][y + 1] != 0:
+                    ans.append(arr[x][y + 1])
+        stack = temp[:]
+        count += 1
 
-print(arr[a-1][b-1])
+    if len(ans) == 1:
+        print(0)
+    else:
+        print(min(ans[1:]))
