e0aa1e3deef958c50700db7d33ffb9ceaa1ee0fb
Seungtae Ryu <81846487+SeungtaeRyu@users.noreply.github.com>
2022-12-19 22:13:36 +0900

    [Gold IV] Title: 이분 그래프, Time: 1444 ms, Memory: 54728 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,18 +1,18 @@
 import sys
 from collections import defaultdict
-sys.setrecursionlimit(1000000)
+sys.setrecursionlimit(1000000)          # 재귀 한계 설정 늘려주기
 input = sys.stdin.readline
 
 k = int(input())
 
-def dfs(u, cur, next):
+def dfs(u, cur, next):                  # 현재 정점, 현재 정점의 색, 다음 정점의 색
     global flag
-    for w in adj[u]:
-        if visited[w] == -1:
-            visited[w] = next
-            dfs(w, next, cur)
-        elif visited[w] == cur:
-            flag = False
+    for w in adj[u]:                    # 현재랑 이어져있는 다음 정점 모두 탐색
+        if visited[w] == -1:            # 아직 방문 안했으면
+            visited[w] = next               # 현재색과 반대되는 색으로 방문체크 후
+            dfs(w, next, cur)               # 계속 dfs 실행
+        elif visited[w] == cur:         # 이미 다음정점이 현재정점이랑 색이 같으면
+            flag = False                    # flag 세우고 종료시키기
             return
 
 for tc in range(k):
@@ -26,12 +26,15 @@ for tc in range(k):
     visited = [-1 for _ in range(v+1)]
     flag = True
 
-    for i in range(1, v+1):
-        if visited[i] == -1:
-            visited[i] = 0
-            dfs(i, 0, 1)
+    for i in range(1, v+1):             # 모든 정점에 대해 탐색
+        if visited[i] == -1:            # 방문안한 점을 찾으면
+            if flag == False:
+                break
+            visited[i] = 0              # 현재 0, 다음 1로 설정하고
+            dfs(i, 0, 1)                # dfs 시작
 
-    if flag:
-        print("YES")
-    else:
-        print("NO")
+    if flag:                # 모든 정점 방문 완료 후 flag가 False로 안바뀌었으면
+        print("YES")            # 성공
+    else:                   # 바뀌었으면
+        print("NO")             # 실패
+
