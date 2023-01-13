457861a9114551c142dc6c8aff8ca16936646e24
Seungtae Ryu <81846487+SeungtaeRyu@users.noreply.github.com>
2023-01-13 13:00:00 +0900

    [Gold IV] Title: 전화번호 목록, Time: 864 ms, Memory: 45960 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -2,8 +2,7 @@ import sys
 input = sys.stdin.readline
 
 class Node(object):
-    def __init__(self, key, data=None):
-        self.key = key
+    def __init__(self, data=None):
         self.data = data
         self.children = {}
 
@@ -15,7 +14,7 @@ def insert(string):
         if char not in current_node.children:
             if current_node.data:
                 flag = False
-            current_node.children[char] = Node(char)
+            current_node.children[char] = Node()
         current_node = current_node.children[char]
     current_node.data = string
 
@@ -24,7 +23,7 @@ for tc in range(1, T+1):
     n = int(input())
     nums = []
 
-    root = Node("head")
+    root = Node()
     flag = True
 
     for i in range(n):
