d16745e6f79dce4ab08c5508141c0c81dc382268
Seungtae Ryu <81846487+SeungtaeRyu@users.noreply.github.com>
2023-01-14 23:33:56 +0900

    [Gold IV] Title: 전화번호 목록, Time: 140 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -1,41 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-class Node(object):
-    def __init__(self, data=None):
-        self.data = data
-        self.children = {}
-
-def insert(string):
-    global flag
-    current_node = root
-
-    for char in string:
-        if char not in current_node.children:
-            if current_node.data:
-                flag = False
-            current_node.children[char] = Node()
-        current_node = current_node.children[char]
-    current_node.data = string
-
-T = int(input())
-for tc in range(1, T+1):
+def sol():
     n = int(input())
-    nums = []
+    db = [input().strip() for _ in range(n)]
+    db.sort()
+    for j in range(n-1):
+        if db[j] == db[j+1][:len(db[j])]:
+            return "NO"
+    return "YES"
 
-    root = Node()
-    flag = True
-
-    for i in range(n):
-        num = input().rstrip()
-        nums.append(num)
-
-    nums.sort(key=lambda x: len(x))
-
-    for num in nums:
-        insert(num)
-
-    if flag:
-        print("YES")
-    else:
-        print("NO")
+t = int(input())
+for _ in range(t):
+    print(sol())
