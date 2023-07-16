fe62c86ccefa24cc7bd6ec5868ecca161e5f88a9
Seungtae Ryu <81846487+SeungtaeRyu@users.noreply.github.com>
2023-07-16 17:25:20 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
@@ -0,0 +1,20 @@
+import math
+
+def solution(n,a,b):
+    answer = 1
+
+    if a > b:
+            a, b = b, a
+
+    while True:
+        if a % 2 == 1 and a+1 == b:
+            break
+        else:
+            if a == 1:
+                pass
+            else:
+                a = math.ceil(a/2)
+            b = math.ceil(b/2)
+            answer += 1
+
+    return answer
